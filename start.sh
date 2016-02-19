8a6c59b424b50c3661dbabcfe28532ae3287f24e
udaysagar2177 <udaysagar.2177@gmail.com>
2016-02-18 20:33:59 -0800

    Replace DynamoDB with MongoDB

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Tip:"
+echo "     Remove mongodb/mongodb_data/db/.mongodb_password_set if you"
+echo "     are changing the password"
+
+MONGODB_USER="adenosine"
+MONGODB_PASS="abcdefghijkl"
+MONGODB_DATABASE="adenosine"
+
 function stop_and_remove_container(){
   if [[ $(docker ps -a | grep -w "$1" | grep -Ev "grep") ]]; then
     echo "killing currently running $1"
@@ -7,6 +15,16 @@ function stop_and_remove_container(){
   fi
 }
 
-stop_and_remove_container "dynamodb_local"
-docker build -t dynamodb dynamodb/
-docker run -d -p 8000:8000 --name "dynamodb_local" -v $(pwd)/dynamodb/dynamodb_data:/dynamodb_data dynamodb
+stop_and_remove_container "mongodb"
+docker build -t mongodb mongodb/
+docker run \
+	-it \
+	-e MONGODB_USER=${MONGODB_USER} \
+	-e MONGODB_PASS=${MONGODB_PASS} \
+	-e MONGODB_DATABASE=$MONGODB_DATABASE \
+	-p 27017:27017 \
+	-p 28017:28017 \
+	-h "mongodb" \
+	--name "mongodb" \
+	-v $(pwd)/mongodb/mongodb_data/db:/data/db \
+	mongodb
